ed4710510cb6337db40e7466fcd7e6460d56ddf8
isahua16 <isaelhuard@gmail.com>
2023-03-23 17:30:46 -0600

    learning query selectors

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let images = document.querySelectorAll(`img`);
+let first_h2 = document.querySelector(`h2`);
+let all_p_tags = document.querySelectorAll(`p`);
+let all_articles = document.querySelectorAll(`.article`);
+let my_header = document.querySelector(`#main_header`);
