b238e4a1a2414649b79df98a69175bf53fd0320f
isahua16 <isaelhuard@gmail.com>
2023-03-23 19:31:57 -0600

    inserting html and setting attributes

--- js/app.js
+++ js/app.js
@@ -16,3 +16,14 @@ for(let i = 0; i < all_p_tags.length; i++)
 {
     all_p_tags[i].innerHTML = `HACKED`;
 }
+
+first_h2.insertAdjacentHTML(`beforebegin`, `<h1>***</h1>`);
+first_h2.insertAdjacentHTML(`afterbegin`, `<h3>***</h3>`);
+first_h2.insertAdjacentHTML(`beforeend`, `<h3>***</h3>`);
+first_h2.insertAdjacentHTML(`afterend`, `<h1>***</h1>`);
+
+
+for(let i = 0; i<images.length; i++)
+{
+    images[i].setAttribute(`src`, `https://img.freepik.com/free-photo/beautiful-pet-portrait-dog_23-2149218450.jpg?w=2000&t=st=1679621275~exp=1679621875~hmac=5eb2b4f84a931cde323452f8bdce27242f0bc8323a018ee8fb468e66dae45de4`);
+}
