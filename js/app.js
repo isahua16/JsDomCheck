f25f55795382b43de8dca265c556d564106e34d7
isahua16 <isaelhuard@gmail.com>
2023-03-23 18:47:16 -0600

    Manipulation styles with JS

--- js/app.js
+++ js/app.js
@@ -6,3 +6,13 @@ let my_header = document.querySelector(`#main_header`);
 
 let all_articles2 = document.getElementsByClassName(`article`);
 let my_header2 = document.getElementById(`main_header`);
+
+first_h2.style.fontSize = `2rem`;
+first_h2.style.color = `green`;
+first_h2.style.backgroundColor = `yellow`;
+
+for(let i = 0; i < all_p_tags.length; i++)
+{
+    all_p_tags[i].innerHTML = `HACKED`;
+}
+
